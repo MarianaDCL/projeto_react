5b7fde32c5c790f22a72f80112d82936cc2db94c
Mariana Lima <marianadecamposlima.ar@gmail.com>
2021-11-03 21:36:16 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import homemFeliz from './imagens/homem-feliz-dinheiro.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={homemFeliz} className="App-logo" alt="homem feliz" />
+    </div>
+  );
+}
+
+export default App;
